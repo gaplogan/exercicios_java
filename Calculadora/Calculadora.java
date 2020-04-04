71cbc7db84659756fd72752f5aa07f9a86a3818b
gaplogan <gaplogan@gmail.com>
2020-04-04 17:37:13 -0300

    v. 2020.04.04.17.37

--- Calculadora/Calculadora.java
+++ Calculadora/Calculadora.java
@@ -1,40 +1,43 @@
-public class Calculadora
-{
-	public static void main(String args[])
-	{
+public class Calculadora{
+	
+	public static void main(String args[]){
+		
 		double num1 = Double.parseDouble(args[0]);
 		double num2 = Double.parseDouble(args[2]);
+		
 		String operacao = args[1];
 		
 		double resultado = 0;
 		
-		switch(operacao)
-		{
+		switch(operacao){
 			default:
-				System.out.println("Erro, opção inválida");
+				System.out.println("Operação Inválida");
 				break;
 			case "+":
 				resultado = num1 + num2;
-				System.out.println("O resultado da soma é: " + resultado);
+				System.out.println("O Resultado da Soma é: " + resultado);
 				break;
 			case "-":
 				resultado = num1 - num2;
-				System.out.println("O resultado da subtração é: " + resultado);
+				System.out.println("O Resultado da Subtração é: " + resultado);
 				break;
 			case "x":
 			case "X":
-			resultado = num1 * num2;
-				System.out.println("O resultado da multiplicação é: " + resultado);
+				resultado = num1 * num2;
+				System.out.println("O Resultado da Multiplicação é: " + resultado);
 				break;
 			case "/":
-			case ":":
+			case ":":				
 				if(num2 == 0){
-					System.out.println("Não é possível dividir por 0 (zero).");					
+					System.out.println("Não é possível dividir por zero.");
 				}else{
-					resultado = num1 / num2;
-					System.out.println("O resultado da divisão é: " + resultado);
-				}
+					resultado = num1 / num2;					
+					System.out.println("O Resultado da Divisão é: " + resultado);
+				}				
 				break;
 		}
 	}
 }
+
+
+
